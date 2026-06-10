f238938c56b2c1d683c7324ef88a931fdbef8bca
Yasmine Mohamed <yasmineraef76@gmail.com>
2026-06-10 10:20:33 -0400

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import fs from "fs";
+
+//Detect if running in WSL by checking for /proc/version
+const isWSL =
+  fs.existsSync("/proc/version") &&
+  fs
+    .readFileSync("/proc/version", "utf-8")
+    .toLocaleLowerCase()
+    .includes("microsoft");
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    watch: {
+      usePolling: isWSL,
+      interval: 200,
+    },
+  },
+});
