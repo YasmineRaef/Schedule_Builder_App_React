ca081c05bffed7e4c113995b1678dc147c60b0a2
Yasmine Mohamed <yasmineraef76@gmail.com>
2026-06-11 07:33:12 -0400

    Modified and styles the added classes heights to render within the timegrid intervals based on the entered class duration

--- src/components/SelectedClass.tsx
+++ src/components/SelectedClass.tsx
@@ -1,12 +1,39 @@
+const GRID_START = timeToMinutes("8:00");
+const PX_PER_MINUTE = 32 / 60;
+const EXTRA_OFFSET = 100;
+
 //@ts-expect-error
 const SelectedClass = ({ name, day, start, end }) => {
+  //Business Logic
+  const startMin = timeToMinutes(start);
+  const endMin = timeToMinutes(end);
+  const classDuration = endMin - startMin;
+  const duration = Math.max(classDuration, 40);
+  const topOffset = EXTRA_OFFSET + (startMin - GRID_START) * PX_PER_MINUTE;
+  const classHeight = duration * PX_PER_MINUTE;
+
   return (
     <>
-      <p className="classObject">
-        {name} - {start} to {end}
-      </p>
+      <div
+        className="classObject"
+        style={{
+          background: "palevioletred",
+          position: "absolute",
+          top: `${topOffset}px`,
+          height: `${classHeight}px`,
+          left: "5px",
+          right: "5px",
+        }}
+      >
+        {name} <br /> {start} - {end}
+      </div>
     </>
   );
 };
+
+function timeToMinutes(duration: string) {
+  const [hour, minute] = duration.split(":").map(Number);
+  return hour * 60 + minute;
+}
 
 export default SelectedClass;
